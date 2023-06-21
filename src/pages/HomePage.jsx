f7066b81c340402378eee76cd808087a25988795
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-21 23:15:02 +0300

    write api function for getting users and updating followers amount

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import { getUsers, updateUserFollowers } from "service/users";
+
 const HomePage = () => {
+
+    useEffect(()=>{
+        getUsers(1, 10).then(res => {
+            console.log(res);
+        }).catch(error => {
+        // handle error
+        });
+        updateUserFollowers(1, 111).then(res => {
+            console.log(res);
+        }).catch(error => {
+        // handle error
+        });
+    })
     return(
         <h1>Homepage</h1>
     );
---
+++ src/service/users.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://64934f44428c3d2035d1a3bf.mockapi.io/users';
+
+export async function getUsers(page, amount) {
+    const { data } = await axios.get(`?page=${page}&limit=${amount}`);
+    return data;
+};
+
+export async function updateUserFollowers(id, newFollowers) {
+    const { data } = await axios.put(`/${id}`, {followers: newFollowers});
+    return data;
+};
