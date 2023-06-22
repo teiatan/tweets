cf8004e3854bb47cdcbef8b43f443966f3519627
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-22 22:07:11 +0300

    add load more button markup styles and functionality

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,32 +1,51 @@
 import { UserCard } from "components/UserCard/UserCard";
 import { useEffect, useState } from "react";
 import { getUsers } from "service/users";
-import { Container } from "./UsersList.styled";
+import { CardsContainer, LoadMoreButton } from "./UsersList.styled";
 
 export const UsersList = () => {
 
     const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const usersPerpage = 6;
+
+    const handleLoadMore = () => {
+        setPage(page+1);
+        getUsers(page+1, usersPerpage).then(res => {
+            setUsers([...users, ...res]);
+            console.log('fetch load more');
+        }).catch(error => {
+        // handle error
+        });
+    };
 
     useEffect(()=>{
-        getUsers(1, 10).then(res => {
+        console.log('effect');
+        getUsers(1, usersPerpage).then(res => {
             setUsers(res);
         }).catch(error => {
         // handle error
         });
-    }, [])
+    }, []);
+
     return (
-        <Container>
-    
-            {users.map(user => {
-                return(
-                    <UserCard 
-                        key={user.id}
-                        tweets={user.tweets}
-                        followers={user.followers}
-                        avatar={user.avatar}
-                    />
-                )
-            })}
-        </Container>
+        <>
+            <CardsContainer>
+                {users.map(user => {
+                    return(
+                        <UserCard 
+                            key={user.id}
+                            tweets={user.tweets}
+                            followers={user.followers}
+                            avatar={user.avatar}
+                        />
+                    )
+                })}
+
+            </CardsContainer>
+            {(page<(100/usersPerpage)) && <LoadMoreButton onClick={handleLoadMore}>Load more</LoadMoreButton>}
+        </>
+
     )
 };
--- src/components/UsersList/UsersList.styled.js
+++ src/components/UsersList/UsersList.styled.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { device } from "utils/mediaQueries";
 
-export const Container = styled.ul`
+export const CardsContainer = styled.ul`
   padding-top: 80px;
   display: flex;
   flex-wrap: wrap;
@@ -11,4 +11,23 @@ export const Container = styled.ul`
   ${device.mobile}{
     justify-content: center;
   }
+`;
+
+export const LoadMoreButton = styled.button`
+  margin: 30px 0;
+  width: 100%;
+  height: 50px;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+  color: #EBD8FF;
+  border: none;
+  border-radius: 10px;
+  text-transform: uppercase;
+  background: linear-gradient(
+            114.99deg,
+            #471ca9 -0.99%,
+            #5736a3 54.28%,
+            #4b2a99 78.99%);;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
 `;
