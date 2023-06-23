20fa6d684ac02cdc3ea9220ef7260c4a50802e01
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-24 00:33:10 +0300

    add custom favicon

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   // <React.StrictMode>
-    <BrowserRouter basename="tweets">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   // </React.StrictMode>
 );
