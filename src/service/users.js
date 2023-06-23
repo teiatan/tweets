a9325af7e1edf9e20fd17c780421b7bbb7532197
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-23 10:44:12 +0300

    Update users.js

--- src/service/users.js
+++ src/service/users.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64934f44428c3d2035d1a3bf.mockapi.io/users';
+axios.defaults.baseURL = 'https://64934f44428c3d2035d1a3bf.mockapi.io/';
 
 export async function getUsers(page, amount) {
-    const { data } = await axios.get(`?page=${page}&limit=${amount}`);
+    const { data } = await axios.get(`users?page=${page}&limit=${amount}`);
     return data;
 };
 
 export async function updateUserFollowers(id, newFollowers) {
-    const { data } = await axios.put(`/${id}`, {followers: newFollowers});
+    const { data } = await axios.put(`users/${id}`, {followers: newFollowers});
     return data;
 };
 
 export async function getUserFollowers(id) {
-    const { data } = await axios.put(`/${id}`);
+    const { data } = await axios.put(`users/${id}`);
     return data.followers;
 };
+
+export async function getUserById(id) {
+    const { data } = await axios.put(`users/${id}`);
+    return data;
+};
+
+export async function getSessionFollowers(id) {
+    const { data } = await axios.put(`sessions/${id}`);
+    return data;
+};
+
+
