7cf3ef927d8cf3b52cb4de1b2be5d62cb636e6b3
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-24 00:24:02 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <BrowserRouter basename="tweets">
       <App />
